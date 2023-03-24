901f3760075f5f05f3d8a832eb0782f799c60db0
Genevieve Ferrari <genferrari@gmail.com>
2023-03-24 16:39:08 -0700

    added pictures, removed tailwind and added antD to dependencies

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
+    
     <div className="aboutMe container mx-auto">
       <h2 className="title font-bold underline">Why isn't this showing up?</h2>
     </div>
@@ -11,3 +12,5 @@ function Footer() {
 export default Footer;
 
 // just getting basic code in the structure 
+
+
