65d543a866868154874472d313d876b7c03d8897
Genevieve Ferrari <genferrari@gmail.com>
2023-03-24 17:52:28 -0700

    created skeleton for about me card npm run start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import { Avatar, Card } from "antd";
 // import Header from "./components/header";
 import aboutMe from "./components/aboutMe";
+import AboutMe from "./components/aboutMe";
 
 function App() {
   return (
     <div className="App">
-      <header className="text-3xl font-bold underline">
+      {/* <header className="text-3xl font-bold underline">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Genevieve Ferrari</p>
         <a
@@ -17,9 +18,9 @@ function App() {
         // target="_blank"
         // rel="noopener noreferrer"
         ></a>
-      </header>
+      </header> */}
       <div>
-        <aboutMe></aboutMe>
+        <AboutMe />
       </div>
     </div>
   );
