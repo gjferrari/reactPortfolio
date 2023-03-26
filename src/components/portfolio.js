14ecd80cbd45d09b0a4dcc0274010e7035ce3190
Genevieve Ferrari <genferrari@gmail.com>
2023-03-25 23:24:41 -0700

    fixed carousel

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,24 +1,35 @@
 import React from "react";
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import { Avatar, Card, Col, Row, Carousel } from "antd";
+import { GithubFilled, LinkedinFilled } from "@ant-design/icons";
+import { Avatar, Card, Carousel, Radio, Button } from "antd";
+import { useState } from "react";
 
 const { Meta } = Card;
 
 const Portfolio = () => {
-  const onChange = (currentSlide) => {
-    console.log(currentSlide);
+  const [dotPosition, setDotPosition] = useState("top");
+  const handlePositionChange = ({ target: { value } }) => {
+    setDotPosition(value);
   };
+  // const onChange = (currentSlide) => {
+  //   console.log(currentSlide);
+  // };
 
   return (
     <div className="Portfolio" style={{ width: 350 }}>
-      <Carousel afterChange={onChange}>
+      <Radio.Group
+        onChange={handlePositionChange}
+        value="top"
+        style={{
+          marginBottom: 8,
+        }}
+      >
+        {/* <Radio.Button value="top"></Radio.Button> */}
+      </Radio.Group>
+      <Carousel autoplay dotPosition="top">
         <div id="caroCard" style={{ width: 300, height: 600 }}>
           <Card
-            style={{ width: 300 }}
+            hoverable={true}
+            style={{ width: 300, display: "flex", flexDirection: "column" }}
             cover={
               <img
                 alt="example"
@@ -26,9 +37,26 @@ const Portfolio = () => {
               />
             }
             actions={[
-              <SettingOutlined key="setting" />,
-              <EditOutlined key="edit" />,
-              <EllipsisOutlined key="ellipsis" />,
+              <Button
+                type="text"
+                icon={
+                  <LinkedinFilled
+                    style={{ color: "#800797", fontSize: "20px" }}
+                    key="link"
+                  />
+                }
+                href="https://www.linkedin.com/in/genevieve-ferrari-17001178/"
+              ></Button>,
+              <Button
+                type="text"
+                icon={
+                  <GithubFilled
+                    style={{ color: "#800797", fontSize: "20px" }}
+                    key="link"
+                  />
+                }
+                href="https://github.com/gjferrari"
+              ></Button>,
             ]}
           >
             <Meta
@@ -36,34 +64,52 @@ const Portfolio = () => {
                 <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
               }
               title="About Me"
-              description="This is the description"
+              description="I'm a full stack web developmer based out of Tacoma, WA"
             />
           </Card>
         </div>
 
         <div style={{ width: 300 }}>
-          <Card
-            style={{ width: 300 }}
-            cover={
-              <img
-                alt="example"
-                src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679707119/StandupShot_r7lkic.jpg"
-              />
-            }
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              avatar={
-                <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
-              }
-              title="About Me"
-              description="This is the description"
-            />
-          </Card>
+        <Card
+    hoverable={true}
+    style={{ width: 300, display: "flex", flexDirection: "column" }}
+    cover={
+      <img
+        alt="example"
+        src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679707119/StandupShot_r7lkic.jpg"
+      />
+    }
+    actions={[
+      <Button
+        type="text"
+        icon={
+          <LinkedinFilled
+            style={{ color: "#800797", fontSize: "20px" }}
+            key="link"
+          />
+        }
+        href="https://www.linkedin.com/in/genevieve-ferrari-17001178/"
+      ></Button>,
+      <Button
+        type="text"
+        icon={
+          <GithubFilled
+            style={{ color: "#800797", fontSize: "20px" }}
+            key="link"
+          />
+        }
+        href="https://github.com/gjferrari"
+      ></Button>,
+    ]}
+  >
+    <Meta
+      avatar={
+        <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
+      }
+      title="About Me"
+      description="I'm a full stack web developmer based out of Tacoma, WA"
+    />
+  </Card>
         </div>
       </Carousel>
     </div>
@@ -71,3 +117,80 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+// const Portfolio = () => {
+//   const [dotPosition, setDotPosition] = useState("top");
+//   const handlePositionChange = ({ target: { value } }) => {
+//     setDotPosition(value);
+//   };
+//   // const onChange = (currentSlide) => {
+//   //   console.log(currentSlide);
+//   // };
+
+//   return (
+//     <div className="Portfolio" style={{ width: 350 }}>
+//       <Radio.Group
+//         onChange={handlePositionChange}
+//         value="top"
+//         style={{
+//           marginBottom: 8,
+//         }}
+//       >
+//         {/* <Radio.Button value="top"></Radio.Button> */}
+//       </Radio.Group>
+//       <Carousel dotPosition="top">
+//         <div id="caroCard" style={{ width: 300, height: 600 }}>
+//           <Card
+//             style={{ width: 300 }}
+//             cover={
+//               <img
+//                 alt="example"
+//                 src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679707119/StandupShot_r7lkic.jpg"
+//               />
+//             }
+//             // actions={[
+//             //   <SettingOutlined key="setting" />,
+//             //   <EditOutlined key="edit" />,
+//             //   <EllipsisOutlined key="ellipsis" />,
+//             // ]}
+//           >
+//             <Meta
+//               avatar={
+//                 <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
+//               }
+//               title="About Me"
+//               description="This is the description"
+//             />
+//           </Card>
+//         </div>
+
+//         <div style={{ width: 300 }}>
+//           <Card
+//             style={{ width: 300 }}
+//             cover={
+//               <img
+//                 alt="example"
+//                 src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679707119/StandupShot_r7lkic.jpg"
+//               />
+//             }
+//             // actions={[
+//             //   <SettingOutlined key="setting" />,
+//             //   <EditOutlined key="edit" />,
+//             //   <EllipsisOutlined key="ellipsis" />,
+//             // ]}
+//           >
+//             <Meta
+//               avatar={
+//                 <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
+//               }
+//               title="About Me"
+//               description="This is the description"
+//             />
+//           </Card>
+//         </div>
+//       </Carousel>
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
