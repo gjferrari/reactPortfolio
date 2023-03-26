e2ce38351a0606387fc400b6db5dd7b5564521f8
Genevieve Ferrari <genferrari@gmail.com>
2023-03-25 21:41:19 -0700

    adding carousel to portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -15,7 +15,32 @@ const Portfolio = () => {
 
   return (
     <div className="Portfolio" style={{ width: 350 }}>
-      <Carousel>
+      <Carousel afterChange={onChange}>
+        <div id="caroCard" style={{ width: 300, height: 600 }}>
+          <Card
+            style={{ width: 300 }}
+            cover={
+              <img
+                alt="example"
+                src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679707119/StandupShot_r7lkic.jpg"
+              />
+            }
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={
+                <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
+              }
+              title="About Me"
+              description="This is the description"
+            />
+          </Card>
+        </div>
+
         <div style={{ width: 300 }}>
           <Card
             style={{ width: 300 }}
@@ -40,31 +65,6 @@ const Portfolio = () => {
             />
           </Card>
         </div>
-
-        {/* <div style={{ width: 300 }}>
-      <Card
-        style={{ width: 300 }}
-        cover={
-          <img
-            alt="example"
-            src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679707119/StandupShot_r7lkic.jpg"
-          />
-        }
-        // actions={[
-        //   <SettingOutlined key="setting" />,
-        //   <EditOutlined key="edit" />,
-        //   <EllipsisOutlined key="ellipsis" />,
-        // ]}
-      >
-        <Meta
-          avatar={
-            <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679704914/Genevieve_Ferrari_Headshot_xulyob.jpg" />
-          }
-          title="About Me"
-          description="This is the description"
-        />
-      </Card>
-    </div> */}
       </Carousel>
     </div>
   );
