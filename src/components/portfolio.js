f5ea793fc068cd7242085284220ac9d36a8ed026
Genevieve Ferrari <genferrari@gmail.com>
2023-03-26 00:06:57 -0700

    background for card - draft

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,7 +2,16 @@ import React from "react";
 import { GithubFilled, LinkedinFilled } from "@ant-design/icons";
 import { Avatar, Card, Carousel, Radio, Button } from "antd";
 import { useState } from "react";
+import SideHustle from "../assets/expensesScreenShot.png";
+import BurnoutBuddy from "../assets/burnout-buddy.png";
 
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 const { Meta } = Card;
 
 const Portfolio = () => {
@@ -25,17 +34,12 @@ const Portfolio = () => {
       >
         {/* <Radio.Button value="top"></Radio.Button> */}
       </Radio.Group>
-      <Carousel autoplay dotPosition="top">
+      <Carousel dotPosition="top">
         <div id="caroCard" style={{ width: 300, height: 600 }}>
           <Card
             hoverable={true}
             style={{ width: 300, display: "flex", flexDirection: "column" }}
-            cover={
-              <img
-                alt="example"
-                src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679707119/StandupShot_r7lkic.jpg"
-              />
-            }
+            cover={<img alt="example" src={SideHustle} />}
             actions={[
               <Button
                 type="text"
@@ -63,53 +67,48 @@ const Portfolio = () => {
               avatar={
                 <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
               }
-              title="About Me"
-              description="I'm a full stack web developmer based out of Tacoma, WA"
+              title="SideHustle"
+              description="Description of app"
             />
           </Card>
         </div>
 
         <div style={{ width: 300 }}>
-        <Card
-    hoverable={true}
-    style={{ width: 300, display: "flex", flexDirection: "column" }}
-    cover={
-      <img
-        alt="example"
-        src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679707119/StandupShot_r7lkic.jpg"
-      />
-    }
-    actions={[
-      <Button
-        type="text"
-        icon={
-          <LinkedinFilled
-            style={{ color: "#800797", fontSize: "20px" }}
-            key="link"
-          />
-        }
-        href="https://www.linkedin.com/in/genevieve-ferrari-17001178/"
-      ></Button>,
-      <Button
-        type="text"
-        icon={
-          <GithubFilled
-            style={{ color: "#800797", fontSize: "20px" }}
-            key="link"
-          />
-        }
-        href="https://github.com/gjferrari"
-      ></Button>,
-    ]}
-  >
-    <Meta
-      avatar={
-        <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
-      }
-      title="About Me"
-      description="I'm a full stack web developmer based out of Tacoma, WA"
-    />
-  </Card>
+          <Card
+            hoverable={true}
+            style={{ width: 300, display: "flex", flexDirection: "column" }}
+            cover={<img alt="example" src={BurnoutBuddy} />}
+            actions={[
+              <Button
+                type="text"
+                icon={
+                  <LinkedinFilled
+                    style={{ color: "#800797", fontSize: "20px" }}
+                    key="link"
+                  />
+                }
+                href="https://www.linkedin.com/in/genevieve-ferrari-17001178/"
+              ></Button>,
+              <Button
+                type="text"
+                icon={
+                  <GithubFilled
+                    style={{ color: "#800797", fontSize: "20px" }}
+                    key="link"
+                  />
+                }
+                href="https://github.com/gjferrari"
+              ></Button>,
+            ]}
+          >
+            <Meta
+              avatar={
+                <Avatar src="https://res.cloudinary.com/da6r9i8q5/image/upload/v1679770193/Genevieve_Ferrari_Headshot_gvyku1.jpg" />
+              }
+              title="Burnout Buddy"
+              description="Burnout Buddy description"
+            />
+          </Card>
         </div>
       </Carousel>
     </div>
