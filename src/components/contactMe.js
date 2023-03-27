c64d800000e2eb5057a689a0ae0381c9a454fa05
Genevieve Ferrari <genferrari@gmail.com>
2023-03-27 00:01:04 -0700

    rewrote code using antD template

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { validateEmail } from "../utils/helpers";
+// import { validateEmail } from "../utils/helpers";
 // import { Avatar, Card, Button } from "antd";
 import { GithubFilled, LinkedinFilled, MailFilled } from "@ant-design/icons";
 
-import { Button, Form, Input, InputNumber } from "antd";
+import { Button, Form, Input } from "antd";
 const layout = {
   labelCol: {
     span: 8,
@@ -26,43 +26,12 @@ const onFinish = (values) => {
   console.log(values);
 };
 
+
+
 const ContactMe = () => {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [errorMessage, setErrorMessage] = useState("");
-  const { name, email, message } = formState;
-
-  function handleSubmit(event) {
-    Event.preventDefault();
-    console.log(formState);
-  }
-
-  function handleChange(event) {
-    if (event.target.name === "email") {
-      const isValid = validateEmail(event.target.value);
-      console.log(isValid);
-      if (!isValid) {
-        setErrorMessage("Your email is invalid. Please try again.");
-      } else {
-        setErrorMessage("");
-      }
-    } else {
-      if (!event.target.value.length) {
-        setErrorMessage(`${event.target.name} is required.`);
-      } else {
-        setErrorMessage("");
-      }
-    }
-
-    if (!errorMessage) {
-      setFormState({ ...formState, [event.target.name]: event.target.value });
-    }
-  }
   return (
-    <Form
+    <div id="contact-form" style={{padding:30}}>
+      <Form
       {...layout}
       name="nest-messages"
       onFinish={onFinish}
@@ -72,7 +41,7 @@ const ContactMe = () => {
       validateMessages={validateMessages}
     >
       <Form.Item
-        name={["user", "name"]}
+        name={["name"]}
         label="Name"
         rules={[
           {
@@ -80,10 +49,13 @@ const ContactMe = () => {
           },
         ]}
       >
-        <Input placeholder="Frodo Baggins" />
+        <Input
+          placeholder="Frodo Baggins"
+          // onBlur={handleChange}
+        />
       </Form.Item>
       <Form.Item
-        name={["user", "email"]}
+        name={["email"]}
         label="Email"
         rules={[
           {
@@ -91,13 +63,16 @@ const ContactMe = () => {
             required: true,
           },
         ]}
-
       >
-        <Input placeholder="ringbearer@gmail.com" />
+        <Input
+          placeholder="ringbearer@gmail.com"
+
+          // onBlur={handleChange}
+        />
       </Form.Item>
 
       <Form.Item name={["user", "message"]} label="Message">
-        <Input.TextArea />
+        <Input.TextArea placeholder="Wow! You are the greatest coder who ever lived!"/>
       </Form.Item>
       <Form.Item
         wrapperCol={{
@@ -110,102 +85,8 @@ const ContactMe = () => {
         </Button>
       </Form.Item>
     </Form>
+    </div>
   );
 };
 
 export default ContactMe;
-
-// function ContactMe() {
-//   const [formState, setFormState] = useState({
-//     name: "",
-//     email: "",
-//     message: "",
-//   });
-//   const [errorMessage, setErrorMessage] = useState("");
-//   const { name, email, message } = formState;
-
-//   function handleSubmit(event) {
-//     Event.preventDefault();
-//     console.log(formState);
-//   }
-
-//   function handleChange(event) {
-//     if (event.target.name === "email") {
-//       const isValid = validateEmail(event.target.value);
-//       console.log(isValid);
-//       if (!isValid) {
-//         setErrorMessage("Your email is invalid. Please try again.");
-//       } else {
-//         setErrorMessage("");
-//       }
-//     } else {
-//       if (!event.target.value.length) {
-//         setErrorMessage(`${event.target.name} is required.`);
-//       } else {
-//         setErrorMessage("");
-//       }
-//     }
-
-//     if (!errorMessage) {
-//       setFormState({ ...formState, [event.target.name]: event.target.value });
-//     }
-//   }
-
-//   return (
-//     <section className="sections contact">
-//       <h2>Contact Me</h2>
-//       <form id="contact-form" onSubmit={handleSubmit}>
-
-//         <div>
-//           <label htmlFor="name">Name:</label>
-//           <br />
-//           <input
-//             type="text"
-//             name="name"
-//             defaultValue={name}
-//             onBlur={handleChange}
-//           />
-//         </div>
-
-//         <div>
-//           <label htmlFor="email">Email address:</label>
-//           <br />
-//           <input
-//             type="email"
-//             name="email"
-//             defaultValue={email}
-//             onBlur={handleChange}
-//           />
-//         </div>
-
-//         <div>
-//           <label htmlFor="message">Message:</label>
-//           <br />
-//           <textarea
-//             name="message"
-//             rows="5"
-//             defaultValue={message}
-//             onBlur={handleChange}
-//           />
-//         </div>
-//         {errorMessage && (
-//           <div>
-//             <p className="error-text">{errorMessage}</p>
-//           </div>
-//         )}
-//         <Button
-//           type="text"
-//           htmlType="submit"
-//           icon={
-//             <MailFilled
-//               style={{ color: "#800797", fontSize: "20px" }}
-//               key="link"
-//             />
-//           }
-//         ></Button>
-//       </form>
-//     </section>
-//   );
-// }
-
-// export default ContactMe;
